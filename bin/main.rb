8fa0496db1d474af5e7c8780920fea5cb30804f3
che30 <che.blancoo58@gmail.com>
2020-11-10 18:02:03 +0100

    create new branch

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,29 @@
 #!/usr/bin/env ruby
 puts 'Hello microverse!'
+# class  Player
+#     @@board=[ 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+#     @@player=1
+#     def token
+#         puts "player 1 uses: (X)\t player 2 uses:(O)"
+#     end
+#     def display_board
+#         puts("  Tic Tac Toe")
+#         print("     |    | \n");
+#     print("  #{@@board[1]}  | #{@@board[2]}  | #{@@board[3]}\n")
+
+#     print ("_____|____|_____\n");
+#     print("     |    |     \n");
+#     print("  #{@@board[4]}  | #{@@board[5]}  | #{@@board[6]}\n")
+
+#     print ("_____|____|_____\n");
+#     print("     |    |     \n");
+#     print("  #{@@board[7]}  | #{@@board[8]}  | #{@@board[9]}\n")
+
+#     print ("_____|____|_____\n")
+#     print("     |    |     \n")
+
+#     end
+   
+# end
+# player1=Player.new
+# player1.display_board
