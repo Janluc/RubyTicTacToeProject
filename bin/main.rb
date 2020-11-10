f9959246ab9155788786af65f8cf0d7918ca37dd
Janluc <Janlucsaneaux@gmail.com>
2020-11-10 15:25:15 -0400

    Fix game loop logic

--- bin/main.rb
+++ bin/main.rb
@@ -1,35 +1,45 @@
 #!/usr/bin/env ruby
-
+players = []
 puts "\nWelcome to Ruby Tic Tac Toe! Please input a name for Player 1 \n \n"
-player_one_name = gets.chomp
+players[0] = gets.chomp
 puts "\nPlease input a name for Player 2 \n \n"
-player_two_name = gets.chomp
+players[1] = gets.chomp
 
-puts "\nPlayer 1: #{player_one_name} and Player 2: #{player_two_name} \n \n"
+puts "\nPlayer 1: #{players[0]} and Player 2: #{players[1]} \n \n"
 
 # Create board
 
-puts "\n#{player_one_name} please choose heads or tails (H/T) \n \n"
+puts "\n#{players[0]} please choose heads or tails (H/T) \n \n"
 
 coin_flip_choice = gets.chomp
 
-puts "\n#{player_one_name} chose #{coin_flip_choice}"
+puts "\n#{players[0]} chose #{coin_flip_choice}"
 
 # flip coin
-
+coin_flip = %w[H T].sample
 # decide if player one wins the coin toss
+if coin_flip != coin_flip_choice
+  puts "#{players[0]} lost the coin flip!"
+  players[0], players[1] = players[1], players[0]
+else
+  puts "#{players[0]} won the coin flip!"
+end
 game_session = true
 while game_session == true
-  puts "\n#{player_one_name} please choose a spot in the board \n"
+  puts "\n#{players[0]} please choose a spot on the board"
   gets.chomp
 
-  puts "\n#{player_two_name} please choose a spot in the board \n"
-  gets.chomp
+  # print board
 
-  # Will loop until condition is met
-  game_session = false
+  puts "#{players[0]} chose a location"
+
+  # check if player won
+  if player_win
+    puts "#{players[0]} has won!"
+    game_session = false
+  end
+
+  # else change turn order
+  players[0], players[1] = players[1], players[0]
+
 end
-
-# print winner or draw
-
-puts "\n#{player_one_name} has won!"
