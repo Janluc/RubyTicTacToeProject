809efb620f8627424ea3838681603297e294f2ba
che30 <che.blancoo58@gmail.com>
2020-11-11 18:47:29 +0100

    Add game loop functionality

--- bin/main.rb
+++ bin/main.rb
@@ -14,8 +14,8 @@ class Player
   end
 end
 class GameManager
-  attr_accessor :game_session, :players
-
+  attr_accessor :game_session, :players,:playerwin
+  $board = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
   @@playerwin = false
   @@game_session = false
@@ -28,7 +28,16 @@ class GameManager
   end
 
   def checkwin
-
+    if ($board[1]==$board[2]&&$board[2]==$board[3]||
+      $board[4]==$board[5]&&$board[5]==$board[6]||
+      $board[7]==$board[8]&&$board[8]==$board[9]||
+      $board[1]==$board[4]&&$board[4]==$board[7]||
+      $board[1]==$board[5]&&$board[5]==$board[9]||
+      $board[3]==$board[5]&&$board[5]==$board[7]||
+      $board[2]==$board[5]&&$board[5]==$board[8]||
+      $board[3]==$board[6]&&$board[6]==$board[9])
+      @@playerwin=true
+    end
   end
 
   def place_token
@@ -55,20 +64,20 @@ class GameManager
   end
 
 end
-  
+
 def display_board
-  board = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
   puts('  Tic Tac Toe')
   print("     |    | \n")
-  print("  #{board[1]}  | #{board[2]}  | #{board[3]}\n")
+  print("  #{$board[1]}  | #{$board[2]}  | #{$board[3]}\n")
 
   print("_____|____|_____\n")
   print("     |    |     \n")
-  print("  #{board[4]}  | #{board[5]}  | #{board[6]}\n")
+  print("  #{$board[4]}  | #{$board[5]}  | #{$board[6]}\n")
 
   print("_____|____|_____\n")
   print("     |    |     \n")
-  print("  #{board[7]}  | #{board[8]}  | #{board[9]}\n")
+  print("  #{$board[7]}  | #{$board[8]}  | #{$board[9]}\n")
 
   print("_____|____|_____\n")
   print("     |    |     \n")
@@ -76,17 +85,23 @@ end
 
 
 
-puts "\nWelcome to Ruby Tic Tac Toe! Please input a name for Player 1 and Symbol\n \n"
+puts "\nWelcome to Ruby Tic Tac Toe! Please input a name for Player 1 \n \n"
 player_one_name = gets.chomp
+puts "Enter your symbol"
 player_one_token = gets.chomp
-puts "\nPlease input a name for Player 2 and Symbol\n \n"
+puts "\nPlease input a name for Player 2 \n \n"
 player_two_name = gets.chomp
+puts "Enter your symbol"
 player_two_token = gets.chomp
+while player_one_token==player_two_token
+  puts " #{player_two_name} symbol already chosen chose another symbol"
+  player_two_token = gets.chomp
+end
 
 game = GameManager.new(Player.new(player_one_name, player_one_token), Player.new(player_two_name, player_two_token))
-puts "\nPlayer 1: #{game.players[0].name} and Player 2: #{game.players[1].name} \n \n"
+puts "\nPlayer 1: #{game.players[0].name} chose #{game.players[0].token}   Player 2: #{game.players[1].name} chose #{game.players[1].token} \n \n"
 
-# Create board
+# Create $board
 
 puts "\n#{game.players[0].name} please choose heads or tails (H/T) \n \n"
 
@@ -110,7 +125,20 @@ sleep(2)
 game.game_session = true
 while game.game_session == true
   display_board
-  get_player_choice(game.players[0].name)
+  choice=get_player_choice(game.players[0].name).to_i
+  while(choice<1||choice>9||$board[choice].is_a?(String))
+    puts "invalid choice"
+    choice=get_player_choice(game.players[0].name).to_i
+  end
+  $board[choice]=game.players[0].token
+  game.checkwin
+  if game.playerwin
+    puts "Congratulations! #{game.player[0].name} you win "
+    game.game_session=false
+  end
+  game.swap_player
+  
+  
 end
 
 
