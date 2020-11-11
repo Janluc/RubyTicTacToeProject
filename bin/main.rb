fa447b14e7ec5054716c93eaf66d9ca8731d973a
Janluc <Janlucsaneaux@gmail.com>
2020-11-11 16:38:05 -0400

    Add error handling and created methods for various parts of game initialization

--- bin/main.rb
+++ bin/main.rb
@@ -1,182 +1,104 @@
 #!/usr/bin/env ruby
+load '../lib/player.rb'
+load '../lib/gamemanager.rb'
 
 def get_player_choice(player_name)
   puts "#{player_name} please choose a valid number from 1-9"
   gets.chomp
 end
 
-class Player
-  attr_accessor :name, :token
-
-  def initialize(name, token)
-    self.name = name
-    self.token = token
-  end
-end
-class GameManager
-  attr_accessor :game_session, :players,:playerwin
-  $board = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-  @@playerwin = false
-  @@game_session = false
-  
-  def initialize(player_one, player_two)
-    @players = []
-    @players[0] = player_one
-    @players[1] = player_two
-    
-  end
-
-  def checkwin
-    if ($board[1]==$board[2]&&$board[2]==$board[3]||
-      $board[4]==$board[5]&&$board[5]==$board[6]||
-      $board[7]==$board[8]&&$board[8]==$board[9]||
-      $board[1]==$board[4]&&$board[4]==$board[7]||
-      $board[1]==$board[5]&&$board[5]==$board[9]||
-      $board[3]==$board[5]&&$board[5]==$board[7]||
-      $board[2]==$board[5]&&$board[5]==$board[8]||
-      $board[3]==$board[6]&&$board[6]==$board[9])
-      @@playerwin=true
-    end
-  end
-
-  def place_token
-  end
-
-  def coin_flip(choice)
-    coin_flip = %w[H T].sample
-    if coin_flip != choice
-      false
-    else
-      true
-    end
-  end
-  def turn
-
-  end
-
-  def swap_player
-    @players[0], @players[1] = @players[1], @players[0]
-  end
-
-  def check_valid_choice
-
-  end
-
-end
-
-def display_board
-
+def display_board(board)
+  system 'clear'
   puts('  Tic Tac Toe')
   print("     |    | \n")
-  print("  #{$board[1]}  | #{$board[2]}  | #{$board[3]}\n")
+  print("  #{board[1]}  | #{board[2]}  | #{board[3]}\n")
 
   print("_____|____|_____\n")
   print("     |    |     \n")
-  print("  #{$board[4]}  | #{$board[5]}  | #{$board[6]}\n")
+  print("  #{board[4]}  | #{board[5]}  | #{board[6]}\n")
 
   print("_____|____|_____\n")
   print("     |    |     \n")
-  print("  #{$board[7]}  | #{$board[8]}  | #{$board[9]}\n")
+  print("  #{board[7]}  | #{board[8]}  | #{board[9]}\n")
 
   print("_____|____|_____\n")
   print("     |    |     \n")
 end
 
+def init_player(player_number)
+  puts "\nPlease input a name for Player #{player_number} \n \n"
+  player_one_name = gets.chomp
+  puts 'Enter your symbol'
+  player_one_token = gets.chomp
 
-
-puts "\nWelcome to Ruby Tic Tac Toe! Please input a name for Player 1 \n \n"
-player_one_name = gets.chomp
-puts "Enter your symbol"
-player_one_token = gets.chomp
-puts "\nPlease input a name for Player 2 \n \n"
-player_two_name = gets.chomp
-puts "Enter your symbol"
-player_two_token = gets.chomp
-while player_one_token==player_two_token
-  puts " #{player_two_name} symbol already chosen chose another symbol"
-  player_two_token = gets.chomp
+  Player.new(player_one_name, player_one_token)
 end
 
-game = GameManager.new(Player.new(player_one_name, player_one_token), Player.new(player_two_name, player_two_token))
-puts "\nPlayer 1: #{game.players[0].name} chose #{game.players[0].token}   Player 2: #{game.players[1].name} chose #{game.players[1].token} \n \n"
-
-# Create $board
-
-puts "\n#{game.players[0].name} please choose heads or tails (H/T) \n \n"
-
-coin_flip_choice = gets.chomp.upcase
-
-puts "\n#{game.players[0].name} chose #{coin_flip_choice}"
-
-flip_result = game.coin_flip(coin_flip_choice)
-
-if !flip_result
-  puts "#{game.players[0].name} lost the coin toss!"
-  game.swap_player
-else
-  puts "#{game.players[0].name} won the coin toss!"
+def token_length(player)
+  until player.token.length == 1
+    puts 'Symbols must be 1 character in length'
+    player.token = gets.chomp
+  end
 end
 
-puts ''
-puts game.players[0].name
+def check_same_token(player_one, player_two)
+  while player_one.token == player_two.token
+    puts "#{player_two.name} symbol already chosen chose another symbol"
+    player_two.token = gets.chomp
+  end
+end
+
+def init_coin_flip(game)
+  puts "\n#{game.players[0].name} please choose heads or tails (H/T) \n \n"
+  coin_flip_choice = gets.chomp.upcase
+  puts "\n#{game.players[0].name} chose #{coin_flip_choice}"
+  flip_result = game.coin_flip(coin_flip_choice)
+  if !flip_result
+    puts "#{game.players[0].name} lost the coin toss!"
+    game.swap_player
+  else
+    puts "#{game.players[0].name} won the coin toss!"
+  end
+end
+
+player_one = init_player(1)
+token_length(player_one)
+
+player_two = init_player(2)
+token_length(player_two)
+check_same_token(player_one, player_two)
+
+game = GameManager.new(player_one, player_two)
+puts "\nPlayer 1: #{game.players[0].name} chose #{game.players[0].token} "
+puts "Player 2: #{game.players[1].name} chose #{game.players[1].token} \n \n"
+
+init_coin_flip(game)
 sleep(2)
 
 game.game_session = true
 while game.game_session == true
-  display_board
-  choice=get_player_choice(game.players[0].name).to_i
-  while(choice<1||choice>9||$board[choice].is_a?(String))
-    puts "invalid choice"
-    choice=get_player_choice(game.players[0].name).to_i
+
+  display_board(game.board)
+  choice = get_player_choice(game.players[0].name).to_i
+  unless game.check_valid_choice(choice)
+    puts 'invalid choice'
+    choice = get_player_choice(game.players[0].name).to_i
   end
-  $board[choice]=game.players[0].token
-  game.checkwin
-  if game.playerwin
-    puts "Congratulations! #{game.player[0].name} you win "
-    game.game_session=false
+
+  game.board[choice] = game.players[0].token
+
+  if game.check_win
+    display_board(game.board)
+    puts "Congratulations! #{game.players[0].name} you win "
+    game.game_session = false
   end
+
+  if game.check_draw
+    display_board(game.board)
+    puts 'Draw Game! Game over!'
+    game.game_session = false
+  end
+
   game.swap_player
-  
-  
+
 end
-
-
-
-
-
-
-
-
-
-
-
-# # game loop
-# game_session = true
-# while game_session == true
-#   system 'clear'
-#   puts 'choose any number from 1-9 inclusive'
-#   # print board
-#   display_board
-#   puts "\n#{players[0]} please choose a spot on the board"
-#   choice = gets.chomp.to_i
-#   while choice < 1 || choice > 9
-#     puts 'chose a valid number for spot'
-#     choice = gets.chomp.to_i
-#   end
-#   puts "#{players[0]} chose a location"
-
-#   # check if player won
-#   if player_win
-#     puts "#{players[0]} has won!"
-#     game_session = false
-#   end
-
-#   if player_draw
-#     puts 'Draw Game!'
-#     game_session = false
-#   end
-#   # else change turn order
-#   players[0], players[1] = players[1], players[0]
-#   sleep(1)
-# end
