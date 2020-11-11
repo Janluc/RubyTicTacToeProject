987c6deefe38a7e5885b298609bc9a3efaf6f9de
Janluc <Janlucsaneaux@gmail.com>
2020-11-11 17:50:39 -0400

    Add option to play again

--- bin/main.rb
+++ bin/main.rb
@@ -61,44 +61,56 @@ def init_coin_flip(game)
   end
 end
 
-player_one = init_player(1)
-token_length(player_one)
+play_again = true
+while play_again == true
+  player_one = init_player(1)
+  token_length(player_one)
 
-player_two = init_player(2)
-token_length(player_two)
-check_same_token(player_one, player_two)
+  player_two = init_player(2)
+  token_length(player_two)
+  check_same_token(player_one, player_two)
 
-game = GameManager.new(player_one, player_two)
-puts "\nPlayer 1: #{game.players[0].name} chose #{game.players[0].token} "
-puts "Player 2: #{game.players[1].name} chose #{game.players[1].token} \n \n"
+  game = GameManager.new(player_one, player_two)
+  puts "\nPlayer 1: #{game.players[0].name} chose #{game.players[0].token} "
+  puts "Player 2: #{game.players[1].name} chose #{game.players[1].token} \n \n"
 
-init_coin_flip(game)
-sleep(2)
+  init_coin_flip(game)
+  sleep(2)
 
-game.game_session = true
-while game.game_session == true
+  game.game_session = true
+  while game.game_session == true
 
-  display_board(game.board)
-  choice = get_player_choice(game.players[0].name).to_i
-  until game.check_valid_choice(choice)
-    puts 'invalid choice'
+    display_board(game.board)
     choice = get_player_choice(game.players[0].name).to_i
+    until game.check_valid_choice(choice)
+      puts 'invalid choice'
+      choice = get_player_choice(game.players[0].name).to_i
+    end
+
+    game.board[choice] = game.players[0].token
+
+    if game.check_win
+      display_board(game.board)
+      puts "Congratulations! #{game.players[0].name} you win "
+      game.game_session = false
+    end
+
+    if game.check_draw
+      display_board(game.board)
+      puts 'Draw Game! Game over!'
+      game.game_session = false
+    end
+
+    game.swap_player
+
   end
 
-  game.board[choice] = game.players[0].token
+  puts 'Would you like to play again? (Y/N)'
+  if gets.chomp.upcase == 'N'
+    puts 'Thank you for playing!'
+    sleep(3)
+    play_again = false
 
-  if game.check_win
-    display_board(game.board)
-    puts "Congratulations! #{game.players[0].name} you win "
-    game.game_session = false
   end
-
-  if game.check_draw
-    display_board(game.board)
-    puts 'Draw Game! Game over!'
-    game.game_session = false
-  end
-
-  game.swap_player
-
+  system 'clear'
 end
