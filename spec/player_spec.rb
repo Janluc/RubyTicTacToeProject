b7cae722d413974d4ed013ff48441d6fe3b6f25d
Janluc <Janlucsaneaux@gmail.com>
2020-12-01 12:41:09 -0400

    Add test cases to gamemanager spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,17 +1,16 @@
 require_relative '../lib/player'
 
 describe Player do
+  let(:player) { Player.new('John', 'x') }
   describe '#initialize' do
     it 'should return a name of player' do
-      player = Player.new('John', 'x')
       expect(player.name).to eql('John')
       expect(player.name).not_to eql('Pete')
     end
 
     it 'return a token of the player' do
-      player = Player.new('John', 'o')
-      expect(player.token).to eql('o')
-      expect(player.token).not_to eql('x')
+      expect(player.token).to eql('x')
+      expect(player.token).not_to eql('o')
     end
   end
 end
